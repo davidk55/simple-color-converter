1d2dcd6010fa3cd5a9911c331e77873f25f42946
David <david.programming@protonmail.com>
2022-07-05 20:39:21 +0200

    remove mocha

--- webpack.config.js
+++ webpack.config.js
@@ -49,11 +49,6 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource'
       },
-      {
-        test: /test\.js/,
-        use: 'mocha-loader',
-        exclude: /node_modules/,
-      }
     ]
   },
   plugins: [
